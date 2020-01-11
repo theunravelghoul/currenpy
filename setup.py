da197deef5ba5bc5f5ea415959cabbaab5440d32
Denis Chuzhov <d.chuzhou@admitad.com>
2020-01-12 02:43:41 +0300

    Version bump.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="currenpy",
-    version="1.0",
+    version="1.0.1",
     packages=["currenpy"],
     url="https://github.com/theunravelghoul/currenpy",
     license="MIT",
