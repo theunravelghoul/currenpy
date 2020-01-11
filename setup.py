f25823a8c3a34f339a16e066bb5827038535124d
Denis Chuzhov <d.chuzhou@admitad.com>
2020-01-12 02:25:29 +0300

    Added some PyPi related stuff.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='currenpy',
+    version='1.0',
+    packages=['currenpy'],
+    url='https://github.com/theunravelghoul/currenpy',
+    license='MIT',
+    author='theunravelghoul',
+    author_email='',
+    description='Python currency management library.'
+)
