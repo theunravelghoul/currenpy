236fb10224d8c2f2f811ebc1ac1c9f85840a7a36
Denis Chuzhov <d.chuzhou@admitad.com>
2020-01-12 07:17:40 +0300

    Exchange operation returns an ExchangeResult instance now.

--- currenpy/exchange.py
+++ currenpy/exchange.py
@@ -1,5 +1,6 @@
 import abc
 import typing
+from dataclasses import dataclass
 from decimal import Decimal
 
 from currenpy.currency import Currency
@@ -16,6 +17,13 @@ class BaseExchangeRatesBackend(metaclass=abc.ABCMeta):
         raise NotImplementedError
 
 
+@dataclass
+class ExchangeResult:
+    source_currency_amount: Decimal
+    target_currency_amount: Decimal
+    exchange_rate: Decimal
+
+
 class Exchange:
     def __init__(self, exchange_rates_backend: BaseExchangeRatesBackend) -> None:
         self.exchange_rates_backend = exchange_rates_backend
@@ -25,12 +33,17 @@ class Exchange:
         source_currency: typing.Union[Currency, str],
         target_currency: typing.Union[Currency, str],
         amount: Decimal = 1.0,
-    ):
+    ) -> ExchangeResult:
         exchange_rate = self.exchange_rates_backend.get_exchange_rate(
             source_currency, target_currency
         )
         if not exchange_rate:
             raise InvalidExchangeRateError
 
-        exchange_result = amount * exchange_rate
+        target_currency_amount = amount * exchange_rate
+        exchange_result = ExchangeResult(
+            source_currency_amount=amount,
+            target_currency_amount=target_currency_amount,
+            exchange_rate=exchange_rate,
+        )
         return exchange_result
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="currenpy",
-    version="1.0.5",
+    version="1.0.6",
     packages=["currenpy"],
     url="https://github.com/theunravelghoul/currenpy",
     license="MIT",
